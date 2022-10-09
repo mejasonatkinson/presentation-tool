ab60c6ceeaabed0d3287e9ab5017fe1847a4ad8e
mejasonatkinson <me@jasonatkinson.co.uk>
2022-10-09 20:29:25 +0000

    more work on combo

--- public/demos/combo/script.js
+++ public/demos/combo/script.js
@@ -11,55 +11,84 @@ let slides = [
             text: "Placeholder 1",
         }
     },
-    {
-        id: "slide-2",
-        layout: "split-left",
-        styling: {
-            fontSize: "21px",
-            fontFamily: "sans-serif",
-            backgroundColor: "blue",
-        },
-        content: {
-            text: "Placeholder 2",
-            image: "https://via.placeholder.com/150x150",
-        }
-    },
 ];
 
 const slideContainer = document.getElementById('slide-container');
-let firstSlide = 1;
 
-slides.forEach(slide => {
+let mainSlide = "slide-1";
+
+function createSlides() {
+
+    document.getElementById("thumbnails").innerHTML = "";
+
+    slides.forEach(slide => {
     
-    const newDiv = document.createElement("div");
-
-    newDiv.classList.add('slide');
-    newDiv.classList.add(slide.layout);
-
-    newDiv.setAttribute('id', slide.id);
-
-    newDiv.style.fontSize = slide.styling.fontSize;
-    newDiv.style.fontFamily = slide.styling.fontFamily;
-    newDiv.style.backgroundColor = slide.styling.backgroundColor;
-
-    newDiv.appendChild(document.createTextNode(slide.content.text));
-
-    document.getElementById('thumbnails').appendChild(newDiv);
-
-    if (firstSlide == 1) {
-        slideContainer.style.fontSize = slide.styling.fontSize;
-        slideContainer.style.fontFamily = slide.styling.fontFamily;
+        const newDiv = document.createElement("div");
+    
+        newDiv.classList.add('slide');
+        newDiv.classList.add(slide.layout);
+    
+        newDiv.setAttribute('id', slide.id);
+    
+        newDiv.style.backgroundColor = slide.styling.backgroundColor;
+    
+        newDiv.appendChild(document.createTextNode(slide.content.text));
+    
+        document.getElementById('thumbnails').appendChild(newDiv);
+    
         slideContainer.style.backgroundColor = slide.styling.backgroundColor;
         slideContainer.textContent = slide.content.text;
-        firstSlide++;
-    }
+    
+        document.getElementById(slide.id).addEventListener('click', function(event) {
+            slideContainer.style.backgroundColor = slide.styling.backgroundColor;
+            slideContainer.textContent = slide.content.text;
 
-    document.getElementById(slide.id).addEventListener('click', function(event) {
-        slideContainer.style.fontSize = slide.styling.fontSize;
-        slideContainer.style.fontFamily = slide.styling.fontFamily;
-        slideContainer.style.backgroundColor = slide.styling.backgroundColor;
-
-        slideContainer.textContent = slide.content.text;
+            mainSlide = slide.id;
+        })
+    
     })
 
-})
+}
+
+createSlides();
+
+const addSlide = document.getElementById('create-slide');
+
+let slideNumber = 2;
+
+addSlide.addEventListener('click', function() {
+
+    let slide = {
+        id: "slide-" + slideNumber,
+        layout: "full-text",
+        styling: {
+            fontSize: "21px",
+            fontFamily: "sans-serif",
+            backgroundColor: "red",
+        },
+        content: {
+            text: "Placeholder " + slideNumber,
+        }
+    };
+
+    slides.push(slide);
+
+    slideNumber++;
+
+    createSlides();
+
+});
+
+const backgroundColour = document.getElementById('background-colour');
+
+backgroundColour.addEventListener('change', function(event) {
+
+    slides.forEach(slide => {
+        if(slide.id == mainSlide) {
+            slide.styling.backgroundColor = event.target.value;
+            slideContainer.style.backgroundColor = event.target.value;
+            createSlides();
+        }
+    });
+
+});
