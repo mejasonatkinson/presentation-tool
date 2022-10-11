2168bbc2f5b4a3e65ac93ca5928c3e3b69cdb950
mejasonatkinson <me@jasonatkinson.co.uk>
2022-10-11 18:53:44 +0000

    Adding comments

--- public/demos/combo/script.js
+++ public/demos/combo/script.js
@@ -1,3 +1,5 @@
+// Create object 
+// & Default first slide
 let slides = [
     {
         id: "slide-1",
@@ -13,36 +15,57 @@ let slides = [
     },
 ];
 
+// Get Main Slider / Selected Slider container
 const slideContainer = document.getElementById('slide-container');
 
+// Set Main Slider / Selected Slider
+// To default first slide
+// ??
 let mainSlide = "slide-1";
 
+// Run create Slide Function
+// Runs:
+// 1. on page load
 function createSlides() {
 
+    // reset the innerHTML of the thumbnails element to nothing
+    // this is to avoid duplicates, WHEN/IF this function is run again
     document.getElementById("thumbnails").innerHTML = "";
 
+    // for each object, in the slides array
     slides.forEach(slide => {
     
+        // create a new div
         const newDiv = document.createElement("div");
     
+        // add 2 classes, a default slide class
+        // & a slide layout class
+        // @TO-DO add more elemnts to help create these layouts
         newDiv.classList.add('slide');
         newDiv.classList.add(slide.layout);
-    
+        // add a slide id
+        // (ref 1)
         newDiv.setAttribute('id', slide.id);
-    
+        // set background colour
         newDiv.style.backgroundColor = slide.styling.backgroundColor;
-    
+        // set text content
         newDiv.appendChild(document.createTextNode(slide.content.text));
-    
+        // include/append the new div to the thumbnails element
         document.getElementById('thumbnails').appendChild(newDiv);
     
+        // For the last object added to the slides array
+        // Set: background colour AND text content; for the Main Slider / Selected Slider container
         slideContainer.style.backgroundColor = slide.styling.backgroundColor;
         slideContainer.textContent = slide.content.text;
     
+        // add a click event listener for each new div with the id set above (ref 1)
         document.getElementById(slide.id).addEventListener('click', function(event) {
+            // Change background colour AND text content; for the Main Slider / Selected Slider container
             slideContainer.style.backgroundColor = slide.styling.backgroundColor;
             slideContainer.textContent = slide.content.text;
 
+            // update the mainSlide variable 
+            // ???
             mainSlide = slide.id;
         })
     
@@ -50,6 +73,7 @@ function createSlides() {
 
 }
 
+// Run create Slide Function
 createSlides();
 
 const addSlide = document.getElementById('create-slide');
@@ -90,6 +114,9 @@ backgroundColour.addEventListener('change', function(event) {
             slide.styling.backgroundColor = event.target.value;
             slideContainer.style.backgroundColor = event.target.value;
             createSlides();
+
+            slideContainer.style.backgroundColor = slide.styling.backgroundColor;
+            slideContainer.textContent = slide.content.text;
         }
     });
 
